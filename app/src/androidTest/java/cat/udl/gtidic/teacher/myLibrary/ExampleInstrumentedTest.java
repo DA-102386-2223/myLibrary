f2640819412d735739b40740c47b9f1c14daee30
diphone-net <43610108+diphone-net@users.noreply.github.com>
2023-03-06 13:12:45 +0100

    fixing Project name

--- app/src/androidTest/java/cat/udl/gtidic/teacher/myLibrary/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cat/udl/gtidic/teacher/myLibrary/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("net.diphone.vl07persistance2", appContext.getPackageName());
+        assertEquals("cat.udl.gtidic.teacher.myLibrary", appContext.getPackageName());
     }
 }
